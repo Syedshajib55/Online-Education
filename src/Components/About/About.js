b0c4914153270f0d95bedfc6742383115328ee31
SyedShajib <syedshajib55@gmail.com>
2021-10-05 23:22:39 +0600

    done

--- src/Components/About/About.js
+++ src/Components/About/About.js
@@ -17,7 +17,7 @@ const About = () => {
                         </div>
                         <div className="col-lg-6">
                             <h1 className="mt-3 titles">Learn With Exclusive Coder <br /> <span className='name'>Education Hub</span> </h1>
-                            <p className="lead  my-5">Overview. Education hus is a platform that allows instructors to build online courses on their preferred topics. Using ours course development tools, they can upload videos, PowerPoint presentations, PDFs, audio, ZIP files and live classes to create courses.</p>
+                            <p className="lead  my-5">Education Hub is a platform that allows instructors to build online courses on their preferred topics. Using ours course development tools, they can upload videos, PowerPoint presentations, PDFs, audio, ZIP files and live classes to create courses.</p>
                         </div>
                     </div>
                 </div>
